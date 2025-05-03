80471d40b4be41884d66493f72b94f23ed62da11
macbook_air <2477952281@qq.com>
2025-05-03 15:17:37 +0800

    fix: 修正RestBean类的包路径

--- src/main/java/com/example/meetingroombookingsystem/utils/RestBean.java
+++ src/main/java/com/example/meetingroombookingsystem/utils/RestBean.java
@@ -1,4 +1,4 @@
-package com.example.meetingroombookingsystem.controller;
+package com.example.meetingroombookingsystem.utils;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.alibaba.fastjson2.JSONWriter;
