1af44587a980c32c4e95512ab8fbbbb3e18ceec3
macbook_air <2477952281@qq.com>
2025-05-04 00:38:44 +0800

    feat: 重构JwtAuthorizeFilter类，优化JWT解析逻辑并添加@Component注解

--- src/main/java/com/example/meetingroombookingsystem/filter/JwtAuthorizeFilter.java
+++ src/main/java/com/example/meetingroombookingsystem/filter/JwtAuthorizeFilter.java
@@ -10,32 +10,30 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthorizeFilter extends OncePerRequestFilter {
     @Resource
     JwtUtils jwtUtils;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+            HttpServletResponse response,
+            FilterChain filterChain)
             throws ServletException, IOException {
         String authorizationHeader = request.getHeader("Authorization");
         DecodedJWT decodedJWT = jwtUtils.resolveToken(authorizationHeader);
-        if(decodedJWT != null) {
+        if (decodedJWT != null) {
             UserDetails user = jwtUtils.toUser(decodedJWT);
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user,
+                    null, user.getAuthorities());
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//            request.setAttribute("id", jwtUtils.toId(decodedJWT));
-
         }
         filterChain.doFilter(request, response);
     }
