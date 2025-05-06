5ab603a1e4e7c62034d5456615a13d67a7ffc3bf
macbook_air <2477952281@qq.com>
2025-05-06 14:26:30 +0800

    chore: 添加MapperScan导入以支持MyBatis映射

--- src/main/java/com/example/meetingroombookingsystem/MeetingRoomBookingSystemApplication.java
+++ src/main/java/com/example/meetingroombookingsystem/MeetingRoomBookingSystemApplication.java
@@ -1,9 +1,11 @@
 package com.example.meetingroombookingsystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.example.meetingroombookingsystem.mapper")
 public class MeetingRoomBookingSystemApplication {
 
     public static void main(String[] args) {
