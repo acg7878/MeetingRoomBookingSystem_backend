0a5e2199997544e444b23a6dd1cd29559591956c
macbook_air <2477952281@qq.com>
2025-05-06 14:24:24 +0800

    feat: 实现用户服务接口，添加根据用户名或邮箱查找用户及根据用户ID查找角色的方法

--- src/main/java/com/example/meetingroombookingsystem/service/UsersService.java
+++ src/main/java/com/example/meetingroombookingsystem/service/UsersService.java
@@ -1,4 +1,11 @@
 package com.example.meetingroombookingsystem.service;
 
-public interface UsersService {
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.meetingroombookingsystem.entity.dto.auth.Users;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UsersService extends IService<Users>, UserDetailsService {
+    Users findUsersByNameOrEmail(String username);
+
+    String findRoleByUserId(Integer userId);
 }
--- src/main/java/com/example/meetingroombookingsystem/service/impl/UsersServiceImpl.java
+++ src/main/java/com/example/meetingroombookingsystem/service/impl/UsersServiceImpl.java
@@ -1,4 +1,63 @@
 package com.example.meetingroombookingsystem.service.impl;
 
-public class UsersServiceImpl {
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.meetingroombookingsystem.entity.dto.auth.Roles;
+import com.example.meetingroombookingsystem.entity.dto.auth.UserRoles;
+import com.example.meetingroombookingsystem.entity.dto.auth.Users;
+import com.example.meetingroombookingsystem.mapper.RolesMapper;
+import com.example.meetingroombookingsystem.mapper.UserRolesMapper;
+import com.example.meetingroombookingsystem.mapper.UsersMapper;
+import com.example.meetingroombookingsystem.service.UsersService;
+
+import jakarta.annotation.Resource;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements UsersService {
+
+    @Resource
+    private UserRolesMapper userRolesMapper;
+
+    @Resource
+    private RolesMapper rolesMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // users 和 user
+        Users users = this.findUsersByNameOrEmail(username); // username可能是用户名也可能是邮箱
+        if (users == null) {
+            throw new UsernameNotFoundException("用户不存在");
+        }
+        return User
+                .withUsername(username)
+                .password(users.getPassword())
+                .roles(findRoleByUserId(users.getUserId()))
+                .build();
+    }
+
+    public Users findUsersByNameOrEmail(String text) {
+        return this.query()
+                .eq("username", text).or()
+                .eq("email", text)
+                .one();
+    }
+
+    public String findRoleByUserId(Integer userId) {
+        // 1. 通过 userId 查找 user_role 中对应的 roleId
+        LambdaQueryWrapper<UserRoles> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(UserRoles::getUserId, userId);
+        UserRoles userRole = userRolesMapper.selectOne(wrapper);
+        if (userRole == null) {
+            return null;
+        }
+        // 2. 通过 roleId 查找 Roles 表
+        Roles role = rolesMapper.selectById(userRole.getRoleId());
+        return role != null ? role.getRoleName() : null;
+    }
+
 }
