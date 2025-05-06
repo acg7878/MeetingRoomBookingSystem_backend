72801a6c3c627657d674cc8f3c401df9cf5203db
macbook_air <2477952281@qq.com>
2025-05-06 20:48:49 +0800

    feat: 实现邮件验证码注册和重置密码功能，新增相关方法和限流机制

--- src/main/java/com/example/meetingroombookingsystem/service/UsersService.java
+++ src/main/java/com/example/meetingroombookingsystem/service/UsersService.java
@@ -2,10 +2,16 @@ package com.example.meetingroombookingsystem.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.meetingroombookingsystem.entity.dto.auth.Users;
+import com.example.meetingroombookingsystem.entity.vo.request.ConfirmResetVO;
+import com.example.meetingroombookingsystem.entity.vo.request.EmailRegisterVO;
+import com.example.meetingroombookingsystem.entity.vo.request.EmailResetVO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UsersService extends IService<Users>, UserDetailsService {
     Users findUsersByNameOrEmail(String username);
-
     String findRoleByUserId(Integer userId);
+    String registerEmailVerifyCode(String type, String email, String address);
+    String registerEmailAccount(EmailRegisterVO info);
+    String resetEmailAccountPassword(EmailResetVO info);
+    String resetConfirm(ConfirmResetVO info);
 }
